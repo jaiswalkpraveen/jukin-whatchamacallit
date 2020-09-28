d2011af3f6d6f41f7babeb3f3e8da56ab5042f9b
Praveen <praveenics>
2020-09-28 22:13:14 +0530

    put webdriver in project to minimize additional installation

--- grailsUIAutomation/src/main/java/com/grails/managers/WebDriverManager.java
+++ grailsUIAutomation/src/main/java/com/grails/managers/WebDriverManager.java
@@ -1,12 +1,9 @@
 package com.grails.managers;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -49,16 +46,8 @@ public class WebDriverManager {
         case FIREFOX : driver = new FirefoxDriver();
 	    	break;
         case CHROME : 
-        	System.setProperty(CHROME_DRIVER_PROPERTY, FileReaderManager.getInstance().getConfigReader().getDriverPath());
-    		ChromeOptions options = new ChromeOptions();
-    		options.setExperimentalOption("useAutomationExtension", false);
-    		options.setExperimentalOption("excludeSwitches",Collections.singletonList("enable-automation")); 
-    		Map<String, Object> prefs = new HashMap<String, Object>();
-    		prefs.put("credentials_enable_service", false);
-    		prefs.put("profile.password_manager_enabled", false);
-    		options.setExperimentalOption("prefs", prefs);
-    		//options.addArguments("--headless");
-        	driver = new ChromeDriver(options);
+        	System.setProperty(CHROME_DRIVER_PROPERTY, System.getProperty("user.dir") + File.separator + FileReaderManager.getInstance().getConfigReader().getDriverPath() + File.separator + "chromedriver");
+        	driver = new ChromeDriver();
         	driver.manage().window().maximize();
 		    driver.manage().timeouts().implicitlyWait(FileReaderManager.getInstance().getConfigReader().getImplicitlyWait(), TimeUnit.SECONDS);
     		break;
@@ -72,5 +61,4 @@ public class WebDriverManager {
 		driver.close();
 		driver.quit();
 	}
-
 }
